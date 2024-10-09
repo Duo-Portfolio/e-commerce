50f889c9c91e7f2f2af070589f17c50c8075ec2b
ahmed-aa1801564 <66021968+AhmedAmmar-ge53qup@users.noreply.github.com>
2024-10-09 23:17:53 +0200

    Fixed a bug with URL names

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -10,11 +10,11 @@ const images = [
     "https://media.sketchfab.com/models/2707c5a2601244daa4226357e63c4f76/thumbnails/379aaf0faf7f4cf592ba7e7acb135585/4590650703d34448ab6ba8e671a0dba0.jpeg",
     "https://img.pikbest.com/wp/202408/sci-fi-metal-sphere-with-glowing-energy-core-3d-rendered-object_9627179.jpg!bw700",
     "https://img.pikbest.com/wp/202408/sci-fi-isolated-hi-tech-container-on-metallic-background-a-futuristic-design-for-military-and-gaming-rendered-in-3d_9794237.jpg!w700wp",
-    "https/images.starfieldwiki.net/thumb/f/fc/SF-prerelease-Semi-Auto-Eon.png/300px-SF-prerelease-Semi-Auto-Eon.png?format=webp&quality=lossless&width=375&height=300",
-    "https/images.starfieldwiki.net/thumb/6/61/SF-item-Deep_Mining_Spacesuit.png/300px-SF-item-Deep_Mining_Spacesuit.png?format=webp&quality=lossless&width=306&height=668",
+    "https://images.starfieldwiki.net/thumb/f/fc/SF-prerelease-Semi-Auto-Eon.png/300px-SF-prerelease-Semi-Auto-Eon.png?format=webp&quality=lossless&width=375&height=300",
+    "https://images.starfieldwiki.net/thumb/6/61/SF-item-Deep_Mining_Spacesuit.png/300px-SF-item-Deep_Mining_Spacesuit.png?format=webp&quality=lossless&width=306&height=668",
     "https://images.starfieldwiki.net/thumb/f/fb/SF-item-Ground_Crew_Space_Helmet.jpg/300px-SF-item-Ground_Crew_Space_Helmet.jpg",
-    "https/images.starfieldwiki.net/thumb/1/15/SF-item-Alien_Genetic_Material.jpg/300px-SF-item-Alien_Genetic_Material.jpg?format=webp&width=375&height=375",
-    "https/images.starfieldwiki.net/thumb/8/8b/SF-item-CoffeeCup_TB_01.png/300px-SF-item-CoffeeCup_TB_01.png?format=webp&quality=lossless&width=375&height=353"
+    "https://images.starfieldwiki.net/thumb/1/15/SF-item-Alien_Genetic_Material.jpg/300px-SF-item-Alien_Genetic_Material.jpg?format=webp&width=375&height=375",
+    "https://images.starfieldwiki.net/thumb/8/8b/SF-item-CoffeeCup_TB_01.png/300px-SF-item-CoffeeCup_TB_01.png?format=webp&quality=lossless&width=375&height=353"
   ];
 
 
@@ -29,8 +29,8 @@ export async function GET() {
         // Parse the JSON data
         const products = JSON.parse(fileData);
 
-        //images.forEach((img, i) => products.products[i].images[0] = img);
-        //await fs.writeFile(filePath, JSON.stringify(products, null, 2), 'utf-8')
+        images.forEach((img, i) => products.products[i].images[0] = img);
+        await fs.writeFile(filePath, JSON.stringify(products, null, 2), 'utf-8')
 
         // If no products are found, send a 404 response
         if (products.length === 0) 
